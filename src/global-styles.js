3d52547c690a3aff8575a8eeae8375387b50f64a
Mihir <tamihir27@yahoo.com>
2020-10-26 12:27:50 +0530

    Added Global Styles

---
+++ src/global-styles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyles } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyles`
+    html, body {
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: black;
+        color: #333333;
+        font-size: 16px;
+    }
+`
