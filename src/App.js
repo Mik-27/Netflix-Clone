521697cf7e2ba629f54551c5cf937af723a23313
Mihir <tamihir27@yahoo.com>
2020-10-28 17:47:13 +0530

    Added Accordion Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import Jumbotron from './components/jumbotron/index';
-import jumboData from './fixtures/jumbotron.json'
+import React from "react";
+import { FaqsContainer } from "./containers/faqs";
+import { FooterContainer } from "./containers/footerContainer";
+import { JumbotronContainer } from "./containers/jumbotronContainer";
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => {
-        console.log(item.id);
-        return(
-          <Jumbotron key={item.id} direction={item.direction}>
-            <Jumbotron.Pane>
-              <Jumbotron.Title>{item.title}</Jumbotron.Title>
-              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            </Jumbotron.Pane>
-            <Jumbotron.Pane>
-              <Jumbotron.Image src={item.image} alt={item.alt}/>
-            </Jumbotron.Pane>
-          </Jumbotron>
-        );
-      })}
-    </Jumbotron.Container>
-  );
+	return (
+		<>
+			<JumbotronContainer />
+			<FaqsContainer />
+			<FooterContainer />
+		</>
+	);
 }
