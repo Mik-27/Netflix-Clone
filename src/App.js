c2a056766926c7a7428cf45115f58e4e510e0555
Mihir <tamihir27@yahoo.com>
2020-11-07 00:43:56 +0530

    Added Protected Routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import Browse from "./pages/browse";
 import Home from "./pages/home";
 import SignIn from "./pages/signin";
 import SignUp from "./pages/signup";
+import { UserRedirect, ProtectedRoute } from "./helpers/routes";
+import useAuthListener from "./hooks/use-auth-listener";
 
 export default function App() {
+	const { user } = useAuthListener();
+	console.log(user);
 	return (
 		<Router>
-			<Route exact path={ROUTES.HOME}>
-				<Home />
-			</Route>
-			<Route exact path={ROUTES.SIGN_IN}>
-				<SignIn />
-			</Route>
-			<Route exact path={ROUTES.SIGN_UP}>
-				<SignUp />
-			</Route>
-			<Route exact path={ROUTES.BROWSE}>
-				<Browse />
-			</Route>
+			<Switch>
+				<UserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.SIGN_IN}
+					exact
+				>
+					<SignIn />
+				</UserRedirect>
+				<UserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.SIGN_UP}
+					exact
+				>
+					<SignUp />
+				</UserRedirect>
+				<ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+					<Browse />
+				</ProtectedRoute>
+				<UserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.HOME}
+					exact
+				>
+					<Home />
+				</UserRedirect>
+			</Switch>
 		</Router>
 	);
 }
