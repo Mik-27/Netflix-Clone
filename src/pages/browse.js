f74429dbf0f2e2885effe1b8ceed83c611663d6d
Mihir <tamihir27@yahoo.com>
2020-11-08 00:27:48 +0530

    Retrieving data from Firebase

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,13 @@
 import React from "react";
+import useContent from "../hooks/use-content";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
+	const { series } = useContent("series");
+	const { films } = useContent("films");
+
+	const slides = selectionFilter({ series, films });
+	console.log(slides);
+
 	return <h1>Browse Page</h1>;
 }
